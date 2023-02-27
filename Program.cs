57cec9bb26ec44a8e4b738df6df6d0a2da47cad6
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-27 13:12:51 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BinarySearchTreeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Bianry search Tree Problems");
+
+            Console.WriteLine("\nCreate Binary Search Tree by adding 56 and then adding 30 & 70\n");
+            BinarySearchTree<int> bst = new BinarySearchTree<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Display();
+            
+        }
+    }
+}
